7ad2d979081405a87c71bedd119d9b7d94402a29
J@tin <ezedin.fedlu@gmail.com>
2021-07-30 19:13:14 +0300

    fix seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Social from "../src/components/Social";
 import GlobalStyle from "../theme/GlobalStyles";
 import theme from "../theme";
+import SearchEngineOptimization from "../src/components/SearchEngineOptimization";
 const StyledContent = styled.div`
   display: flex;
   flex-direction: column;
@@ -12,6 +13,12 @@ const StyledContent = styled.div`
 const Home = () => {
   return (
     <div id="root">
+      <SearchEngineOptimization
+        title={"Ezedin Fedlu - Software Engineer"}
+        siteUrl={"https://ezedin.fedlu.com/"}
+        image={"https://avatars.githubusercontent.com/u/19648503"}
+        description="I'm software engineer who specializes in building (and occasionally designing) exceptional digital experiences."
+      />
       <GlobalStyle bp={theme.bp} mixins={theme.mixins} />
       <StyledContent>
         <Social isHome={true} />
