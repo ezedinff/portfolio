6f86cebcddb7a8baafdf76b4faec77c3bbb552ff
J@tin <ezedin.fedlu@gmail.com>
2021-12-26 12:37:39 +0300

    quick fix

--- src/components/sections/Jobs/index.tsx
+++ src/components/sections/Jobs/index.tsx
@@ -30,22 +30,22 @@ const Jobs = () => {
                 sr().reveal(revealContainer.current, srConfig())
             }
         }
-        animate()
+       animate()
     }, []);
 
     const focusTab = () => {
-        if (tabs.current[tabFocus]) {
-            tabs.current[tabFocus].focus();
-            return;
-        }
-        // If we're at the end, go to the start
-        if (tabFocus >= tabs.current.length) {
-            setTabFocus(0);
-        }
-        // If we're at the start, move to the end
-        if (tabFocus < 0) {
-            setTabFocus(tabs.current.length - 1);
-        }
+        // if (tabs.current[tabFocus]) {
+        //     tabs.current[tabFocus].focus();
+        //     return;
+        // }
+        // // If we're at the end, go to the start
+        // if (tabFocus >= tabs.current.length) {
+        //     setTabFocus(0);
+        // }
+        // // If we're at the start, move to the end
+        // if (tabFocus < 0) {
+        //     setTabFocus(tabs.current.length - 1);
+        // }
     };
 
     // Only re-run the effect if tabFocus changes
