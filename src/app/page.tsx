34db081bd8979259f1ffbced6d87ee48b161fdc8
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2023-11-19 18:52:04 +0300

    initial game structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,260 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  return <Table />;
+}
+
+function Table() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current as unknown as HTMLCanvasElement;
+    if (!canvas) return;
+    canvas.width = 750;
+    canvas.height = 585;
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+    contextRef.current = context;
+
+    const leftPaddle = new Paddle(
+      2 * 15,
+      canvas.height / 2 - 80 / 2,
+      15,
+      80,
+      "white",
+      context,
+      canvas
+    );
+    const rightPaddle = new Paddle(
+      canvas.width - 3 * 15,
+      canvas.height / 2 - 80 / 2,
+      15,
+      80,
+      "white",
+      context,
+      canvas
+    );
+    const ball = new Ball(
+      canvas.width / 2,
+      canvas.height / 2,
+      15,
+      "white",
+      context,
+      canvas,
+      leftPaddle,
+      rightPaddle
+    );
+
+    const animate = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Draw border
+      context.strokeStyle = "white";
+      context.strokeRect(0, 0, canvas.width, canvas.height);
+
+      // Draw vertical line in the middle
+      context.beginPath();
+      context.moveTo(canvas.width / 2, 0);
+      context.lineTo(canvas.width / 2, canvas.height);
+      context.stroke();
+
+      leftPaddle.update();
+      rightPaddle.update();
+      ball.update();
+
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <canvas ref={canvasRef} style={{ border: "1px solid black" }}></canvas>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+class Ball {
+  x: number;
+  y: number;
+  radius: number;
+  color: string;
+  dx: number;
+  dy: number;
+  context: CanvasRenderingContext2D;
+  canvas: HTMLCanvasElement;
+  leftPaddle: Paddle;
+  rightPaddle: Paddle;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  constructor(
+    x: number,
+    y: number,
+    radius: number,
+    color: string,
+    context: CanvasRenderingContext2D,
+    canvas: HTMLCanvasElement,
+    leftPaddle: Paddle,
+    rightPaddle: Paddle
+  ) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.dx = 5; // Change in x
+    this.dy = -5; // Change in y
+    this.context = context;
+    this.canvas = canvas;
+    this.leftPaddle = leftPaddle;
+    this.rightPaddle = rightPaddle;
+  }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  draw() {
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.context.fillStyle = this.color;
+    this.context.fill();
+    this.context.closePath();
+  }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  update() {
+    this.draw();
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    this.x += this.dx;
+    this.y += this.dy;
+
+    // Handle ball collisions with walls
+    if (this.x + this.radius > this.canvas.width || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y - this.radius < 0 || this.y + this.radius > this.canvas.height) {
+      this.dy = -this.dy;
+    }
+
+    // Check for goal and reset the ball
+    if (this.x < 0 || this.x > this.canvas.width) {
+      this.reset();
+    }
+
+    // Check for ball collisions with paddles
+    if (
+      this.collidesWithPaddle(this.leftPaddle) ||
+      this.collidesWithPaddle(this.rightPaddle)
+    ) {
+      this.dx = -this.dx;
+    }
+  }
+
+  collidesWithPaddle(paddle: Paddle) {
+    return (
+      this.x + this.radius > paddle.x &&
+      this.x - this.radius < paddle.x + paddle.width &&
+      this.y + this.radius > paddle.y &&
+      this.y - this.radius < paddle.y + paddle.height
+    );
+  }
+
+  reset() {
+    this.x = this.canvas.width / 2;
+    this.y = this.canvas.height / 2;
+  }
+}
+
+class Paddle {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color: string;
+  dy: number;
+  context!: CanvasRenderingContext2D;
+  canvas!: HTMLCanvasElement;
+
+  constructor(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    color: string,
+    context: CanvasRenderingContext2D,
+    canvas: HTMLCanvasElement
+  ) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.dy = 0;
+    this.context = context;
+    this.canvas = canvas;
+
+    // Event listeners for paddle movement
+    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
+
+  draw() {
+    this.context.beginPath();
+    this.context.rect(this.x, this.y, this.width, this.height);
+    this.context.fillStyle = this.color;
+    this.context.fill();
+    this.context.closePath();
+  }
+
+  update() {
+    this.draw();
+
+    // Move the paddle based on keyboard input
+    if (this.dy < 0 && this.y > 0) {
+      this.y += this.dy;
+    } else if (this.dy > 0 && this.y + this.height < this.canvas.height) {
+      this.y += this.dy;
+    }
+  }
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "ArrowUp") {
+      this.dy = -6; // Adjust the paddle speed if needed
+    } else if (event.key === "ArrowDown") {
+      this.dy = 6;
+    }
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+      this.dy = 0;
+    }
+  };
+}
+
+class Score {
+  x: number;
+  y: number;
+  value: number;
+  context!: CanvasRenderingContext2D;
+  constructor(x: number, y: number, context: CanvasRenderingContext2D) {
+    this.x = x;
+    this.y = y;
+    this.value = 0;
+    this.context = context;
+  }
+
+  draw(context: CanvasRenderingContext2D) {
+    context.font = "20px Arial";
+    context.fillStyle = "black";
+    context.fillText(`Score: ${this.value}`, this.x, this.y);
+  }
+
+  increase() {
+    this.value += 1;
+  }
+
+  update() {
+    this.draw(this.context);
+  }
+
+  reset() {
+    this.value = 0;
+  }
 }
