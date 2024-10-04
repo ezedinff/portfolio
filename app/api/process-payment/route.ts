ab25b0851a238baf09f6275e40324eae5ed3a737
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2024-10-04 21:37:14 +0200

    update

--- app/api/process-payment/route.ts
+++ app/api/process-payment/route.ts
@@ -1,24 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server';
-import Stripe from 'stripe';
+// import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-09-30.acacia',
-});
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: '2024-09-30.acacia',
+// });
+
+// export async function POST(req: NextRequest) {
+//   const { paymentMethodId, amount } = await req.json();
+
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount,
+//       currency: 'usd',
+//       payment_method: paymentMethodId,
+//       confirm: true,
+//     });
+
+//     return NextResponse.json({ clientSecret: paymentIntent.client_secret });
+//   } catch (err) {
+//     const error = err as Error;
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
 
 export async function POST(req: NextRequest) {
-  const { paymentMethodId, amount } = await req.json();
-
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: 'usd',
-      payment_method: paymentMethodId,
-      confirm: true,
-    });
-
-    return NextResponse.json({ clientSecret: paymentIntent.client_secret });
-  } catch (err) {
-    const error = err as Error;
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+    const { paymentMethodId, amount } = await req.json();
+    return NextResponse.json({ paymentMethodId, amount });
 }
