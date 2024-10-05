c4b16e0d5efae81a3d19a83ff194f238aa4e39cb
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2024-10-05 21:37:13 +0200

    Added the Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,6 +17,7 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+const GTM_ID = "GTM-W66RD5MX";
 
 export const metadata: Metadata = {
   title: "TechPro Support | Expert Tech Support for Professionals",
@@ -60,10 +62,27 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="canonical" href="https://techpro.ezedinfedlu.com/" />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${GTM_ID}');
+  `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <JsonLd<LocalBusiness>
           item={{
             "@context": "https://schema.org",
