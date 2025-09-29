ab0f8cd1f26988ef1a20a9f7c7d0f96e075f3f27
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2025-09-29 11:33:42 +0200

    fix

--- src/components/icons/icon.tsx
+++ src/components/icons/icon.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {
   IconAppStore,
   IconBookmark,
@@ -52,8 +51,6 @@ const Icon = ({ name }: {name: string}) => {
   }
 };
 
-Icon.propTypes = {
-  name: PropTypes.string.isRequired,
-};
+
 
 export default Icon;
